5a4aa704a29c4f371136329c8d6d6cbf2a05fa79
jpmalone <jpmalone@iu.edu>
2025-02-18 10:14:22 -0500

    different accuraacy threshold

--- PreviousYearWork/accuracy_metrics.py
+++ PreviousYearWork/accuracy_metrics.py
@@ -98,7 +98,7 @@ def check_results(results_path, label_dict):
             pred, image, conf = split_line
             label = label_dict[image]
             
-            if int(conf) < 917:
+            if int(conf) < 900:
                 manual += 1
                 num_records-=1
             else:
